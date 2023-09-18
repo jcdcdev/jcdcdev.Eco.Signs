3a1b3a624b909c7e282f77e32c07fde4cd689cf0
jcdcdev <james@jcdc.dev>
2023-09-18 01:32:59 +0100

    feature: online players count

---
+++ src/OnlineCountSign.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using Over9000SignPowerMod.Plugins.Interfaces;
+using Eco.Gameplay.Objects;
+using Eco.Gameplay.Players;
+using Eco.Gameplay.Utils;
+
+namespace jcdcdev.Eco.Signs;
+
+public class OnlineCountSign : IOver9000SignPowerModPluginTag
+{
+    public bool CanProcessed(WorldObject worldObject, User registrar)
+    {
+        return true;
+    }
+
+    public string ProcessTag(Match match, int tagNum, WorldObject worldObject, User registrar) =>
+        UserManager.OnlineUsers.Count.ToString();
+
+    public string TagName => "onlineCount";
+    public bool Enabled => true;
+
+    public Regex TagRegex => new Regex("</onlineCount>", RegexOptions.Compiled);
+}
