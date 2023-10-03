a5a54b46277558466dddc3214367d00e7881c194
jcdcdev <james@jcdc.dev>
2023-10-03 01:24:40 +0100

    feature: calorie cost supports out of stock and store id

--- src/Patterns/CalorieCost.cs
+++ src/Patterns/CalorieCost.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.RegularExpressions;
 using Eco.Gameplay.Objects;
 using Eco.Gameplay.Players;
+using jcdcdev.Eco.Core.Services;
 using Over9000SignPowerMod.Plugins.Interfaces;
 
 namespace jcdcdev.Eco.Signs.Patterns;
@@ -11,11 +12,31 @@ public class CalorieCost : IOver9000SignPowerModPluginTag
 
     public string ProcessTag(Match match, int tagNum, WorldObject worldObject, User registrar)
     {
-        var avg = StoreController.Data.AvgCostPerThousandCalories;
+        var oos = match.Value.Contains("oos");
+        var id = Constants.RegexPatterns.Guid.Match(match.Value);
+        float? avg;
+
+        if (Guid.TryParse(id.Value, out var guid))
+        {
+            var store = StoreService.Data.Get(guid);
+            if (store == null)
+            {
+                return Constants.Errors.StoreNotFound;
+            }
+
+            avg = oos ? store.AvgCostPerThousandCalories : store.AvgCostPerThousandCaloriesInStock;
+        }
+        else
+        {
+            avg = oos
+                ? StoreService.Data.AvgCostPerThousandCalories
+                : StoreService.Data.AvgCostPerThousandCaloriesInStock;
+        }
+
         return avg.GetValueOrDefault(-1).ToString("F2");
     }
-    
+
     public string TagName => "calorieCost";
     public bool Enabled => true;
-    public Regex TagRegex => new("</calorieCost>", RegexOptions.Compiled);
+    public Regex TagRegex => new("</calorieCost(.*?)>", RegexOptions.Compiled);
 }
